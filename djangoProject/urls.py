cb69240f2f5937e89db1eca9f484bdc88442f88e
zz <stephen_30zz@163.com>
2023-05-15 22:59:56 +0800

    userprofile , usergister, userlogin 初始化+迁移数据库

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='')),
+
     path('admin/', admin.site.urls),
     path('customer/', include('customer.urls')),
     path('register/', include('userregister.urls')),
