5b06c349a437875f908b267e73114fedff1b86bd
zz <stephen_30zz@163.com>
2023-05-15 19:17:21 +0800

    customer 的搭建 FileDownloadView（未实现）+ 带参数的的请求（未实现）

--- customer/urls.py
+++ customer/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
 
     path(r'', views.APIViewSet.as_view({"get": "list", "post": "create"}), name="customer"),
     path(r'file/', views.FileDownloadView.as_view({"get": "list"}), name="customerfiledownload"),
-
+    re_path(r'^(?P<pk>\d+)/$', views.APIViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy'
+    }), name="customer_1")
 ]
